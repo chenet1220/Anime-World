dc0ecd0a34b2ac46e5ea42b90beab5efc92b4a20
chenet1220 <chrischenet1@gmail.com>
2024-09-16 18:04:26 -0400

    in progress

--- backend/controllers/anime.js
+++ backend/controllers/anime.js
@@ -25,35 +25,35 @@ async function searchAnime(keyword) {
 }
 
 //SHOW GAME FROM RAWG
-async function Show(req, res) {
-    try {
-      const Anime = await fetch(`${baseURL}/anime/${req.params.id}?key=${API_KEY}`);
-      const animeJson = await Anime.json();
-      const dbanime = await Anime.findOne({ rawgId: req.params.id }).populate('reviews.user');
-      if (dbAnime) {
-        return res.status(200).json({
-          Data: animeJson,
-          reviews: dbAnime.reviews 
-        });
-      } else {
-        return res.status(200).json({ Data: animeJson });
-      }
-    } catch (err) {
-      res.status(500).json(err);
-    }
-  };
+// async function Show(req, res) {
+//     try {
+//       const Anime = await fetch(`${baseURL}/anime/${req.params.id}?key=${API_KEY}`);
+//       const animeJson = await Anime.json();
+//       const dbanime = await Anime.findOne({ rawgId: req.params.id }).populate('reviews.user');
+//       if (dbAnime) {
+//         return res.status(200).json({
+//           Data: animeJson,
+//           reviews: dbAnime.reviews 
+//         });
+//       } else {
+//         return res.status(200).json({ Data: animeJson });
+//       }
+//     } catch (err) {
+//       res.status(500).json(err);
+//     }
+//   };
   
-  async function showLikedAnime(req, res) {
-    try {
-      const user = await User.findById(req.user._id).populate('likes');
-      if (!user || !user.likes.length) {
-        return res.status(404).json({ message: 'No liked anime found.' });
-      }
-      res.status(200).json(user.likes);
-    } catch (err) {
-      res.status(500).json({ error: 'Server error', details: err });
-    }
-  }
+//   async function showLikedAnime(req, res) {
+//     try {
+//       const user = await User.findById(req.user._id).populate('likes');
+//       if (!user || !user.likes.length) {
+//         return res.status(404).json({ message: 'No liked anime found.' });
+//       }
+//       res.status(200).json(user.likes);
+//     } catch (err) {
+//       res.status(500).json({ error: 'Server error', details: err });
+//     }
+//   }
 
 
 // Export the functions to be used in other modules
