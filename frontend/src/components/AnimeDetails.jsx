21f528277492e1b447adcd49e73cf7aac93a5639
chenet1220 <chrischenet1@gmail.com>
2024-09-16 13:16:17 -0400

    in progress

--- frontend/src/components/AnimeDetails.jsx
+++ frontend/src/components/AnimeDetails.jsx
@@ -1,7 +1,26 @@
 import React, { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
 import axios from 'axios';
 
-// all const for the api
-// const BASE_URL 
-// const API_KEY 
-// const POSTER_PATH   
+const AnimeDetails = () => {
+    const { id } = useParams();
+    const [anime, setAnime] = useState(null);
+    
+    useEffect(() => {
+        axios.get(`http://localhost:5173/api/anime/${id}`)
+        .then((response) => {
+            setAnime(response.data);
+        });
+    }, [id]);
+    
+    return (
+        <div>
+        {anime && (
+            <div>
+            <h1>{anime.title}</h1>
+            <p>{anime.description}</p>
+            </div>
+        )}
+        </div>
+    );
+    }
